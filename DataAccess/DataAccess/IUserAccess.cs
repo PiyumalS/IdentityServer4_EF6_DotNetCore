75d3d988d085df9ba63f4af67532324fb4976ac6
PiyumalS <piyumalrc@gmail.com>
2017-07-19 16:42:55 +0530

    UserAccess Class Updated

--- DataAccess/DataAccess/IUserAccess.cs
+++ DataAccess/DataAccess/IUserAccess.cs
@@ -18,6 +18,20 @@ namespace DataAccess.DataAccess
         Task<Tuple<bool, string[]>> DeleteUserAsync(string userId);
         Task<Tuple<bool, string[]>> LoginAsync(UserDTO user);
         UserDTO FindUserByID(UserDTO user);
-
+        Task<ApplicationRole> GetRoleByIdAsync(string roleId);
+        Task<ApplicationRole> GetRoleByNameAsync(string roleName);
+        Task<ApplicationRole> GetRoleLoadRelatedAsync(string roleName);
+        Task<List<ApplicationRole>> GetRolesLoadRelatedAsync(int page, int pageSize);
+        Task<Tuple<ApplicationUser, string[]>> GetUserAndRolesAsync(string userId);
+        Task<ApplicationUser> GetUserByEmailAsync(string email);
+        Task<ApplicationUser> GetUserByIdAsync(string userId);
+        Task<ApplicationUser> GetUserByUserNameAsync(string userName);
+        Task<IList<string>> GetUserRolesAsync(UserDTO user);
+        Task<List<Tuple<ApplicationUser, string[]>>> GetUsersAndRolesAsync(int page, int pageSize);
+        Task<Tuple<bool, string[]>> ResetPasswordAsync(UserDTO user, string newPassword);
+        Task<Tuple<bool, string[]>> UpdatePasswordAsync(UserDTO user, string currentPassword, string newPassword);
+        Task<Tuple<bool, string[]>> UpdateRoleAsync(RoleDTO role, IEnumerable<string> claims);
+        Task<Tuple<bool, string[]>> UpdateUserAsync(UserDTO user);
+        Task<Tuple<bool, string[]>> UpdateUserAsync(UserDTO user, IEnumerable<string> roles);
     }
 }
--- DataAccess/DataAccess/UserAccess.cs
+++ DataAccess/DataAccess/UserAccess.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -142,6 +143,7 @@ namespace DataAccess.DataAccess
 
             return Tuple.Create(true, new string[] { });
         }
+
         public UserDTO FindUserByID(UserDTO user)
         {
             ObjectMapper mapper = new ObjectMapper();
@@ -209,6 +211,217 @@ namespace DataAccess.DataAccess
             errorList.Add(existingUser.Id);
             return Tuple.Create(true, errorList.ToArray());
 
+        }
+
+        public async Task<ApplicationRole> GetRoleByIdAsync(string roleId)
+        {
+            return await _appRoleManager.FindByIdAsync(roleId);
+        }
+
+        public async Task<ApplicationRole> GetRoleByNameAsync(string roleName)
+        {
+            return await _appRoleManager.FindByNameAsync(roleName);
+        }
+
+        public async Task<ApplicationRole> GetRoleLoadRelatedAsync(string roleName)
+        {
+            var role = await _appRoleManager.Roles
+                .Include(r => r.Users)
+                .Where(r => r.Name == roleName)
+                .FirstOrDefaultAsync();
+
+            return role;
+        }
+
+        public async Task<List<ApplicationRole>> GetRolesLoadRelatedAsync(int page, int pageSize)
+        {
+            IQueryable<ApplicationRole> rolesQuery = _appRoleManager.Roles
+                .Include(r => r.Users)
+                .OrderBy(r => r.Name);
+
+            if (page != -1)
+                rolesQuery = rolesQuery.Skip((page - 1) * pageSize);
+
+            if (pageSize != -1)
+                rolesQuery = rolesQuery.Take(pageSize);
+
+            var roles = await rolesQuery.ToListAsync();
+
+            return roles;
+        }
+
+        public async Task<Tuple<ApplicationUser, string[]>> GetUserAndRolesAsync(string userId)
+        {
+            var user = await _appUserManager.Users
+                .Include(u => u.Roles)
+                .Where(u => u.Id == userId)
+                .FirstOrDefaultAsync();
+
+            if (user == null)
+                return null;
+
+            var userRoleIds = user.Roles.Select(r => r.RoleId).ToList();
+
+            var roles = await _appRoleManager.Roles
+                .Where(r => userRoleIds.Contains(r.Id))
+                .Select(r => r.Name)
+                .ToArrayAsync();
+
+            return Tuple.Create(user, roles);
+        }
+
+        public async Task<ApplicationUser> GetUserByEmailAsync(string email)
+        {
+            return await _appUserManager.FindByEmailAsync(email);
+        }
+
+        public async Task<ApplicationUser> GetUserByIdAsync(string userId)
+        {
+            return await _appUserManager.FindByIdAsync(userId);
+        }
+
+        public async Task<ApplicationUser> GetUserByUserNameAsync(string userName)
+        {
+            return await _appUserManager.FindByNameAsync(userName);
+        }
+
+        public async Task<IList<string>> GetUserRolesAsync(UserDTO user)
+        {
+            return await _appUserManager.GetRolesAsync(user.Id);
+        }
+
+        public async Task<List<Tuple<ApplicationUser, string[]>>> GetUsersAndRolesAsync(int page, int pageSize)
+        {
+            IQueryable<ApplicationUser> usersQuery = _appUserManager.Users
+                .Include(u => u.Roles)
+                .OrderBy(u => u.UserName);
+
+            if (page != -1)
+                usersQuery = usersQuery.Skip((page - 1) * pageSize);
+
+            if (pageSize != -1)
+                usersQuery = usersQuery.Take(pageSize);
+
+            var users = await usersQuery.ToListAsync();
+
+            var userRoleIds = users.SelectMany(u => u.Roles.Select(r => r.RoleId)).ToList();
+
+            var roles = await _appRoleManager.Roles
+                .Where(r => userRoleIds.Contains(r.Id))
+                .ToArrayAsync();
+
+            return users.Select(u => Tuple.Create(u,
+                roles.Where(r => u.Roles.Select(ur => ur.RoleId).Contains(r.Id)).Select(r => r.Name).ToArray()))
+                .ToList();
+        }
+
+        public async Task<Tuple<bool, string[]>> ResetPasswordAsync(UserDTO user, string newPassword)
+        {
+            string resetToken = await _appUserManager.GeneratePasswordResetTokenAsync(user.Id);
+
+            var result = await _appUserManager.ResetPasswordAsync(user.Id, resetToken, newPassword);
+            if (!result.Succeeded)
+                return Tuple.Create(false, result.Errors.ToArray());
+
+            return Tuple.Create(true, new string[] { });
+        }
+
+        public async Task<Tuple<bool, string[]>> UpdatePasswordAsync(UserDTO user, string currentPassword, string newPassword)
+        {
+            var result = await _appUserManager.ChangePasswordAsync(user.Id, currentPassword, newPassword);
+            if (!result.Succeeded)
+                return Tuple.Create(false, result.Errors.ToArray());
+
+            return Tuple.Create(true, new string[] { });
+        }
+
+        public async Task<Tuple<bool, string[]>> UpdateRoleAsync(RoleDTO role, IEnumerable<string> claims)
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            ApplicationRole appRole = mapper.ConvertRoleToIdentityRole(role);
+
+            if (claims != null)
+            {
+                //string[] invalidClaims = claims.Where(c => ApplicationPermissions.GetPermissionByValue(c) == null).ToArray();
+                //if (invalidClaims.Any())
+                //    return Tuple.Create(false, new string[] { "The following claim types are invalid: " + string.Join(", ", invalidClaims) });
+            }
+
+            var result = await _appRoleManager.UpdateAsync(appRole);
+            if (!result.Succeeded)
+                return Tuple.Create(false, result.Errors.ToArray());
+
+
+            if (claims != null)
+            {
+                //var roleClaims = (await _appRoleManager.GetClaimsAsync(role)).Where(c => c.Type == CustomClaimTypes.Permission);
+                //var roleClaimValues = roleClaims.Select(c => c.Value).ToArray();
+
+                //var claimsToRemove = roleClaimValues.Except(claims).ToArray();
+                //var claimsToAdd = claims.Except(roleClaimValues).Distinct().ToArray();
+
+                //if (claimsToRemove.Any())
+                //{
+                //    foreach (string claim in claimsToRemove)
+                //    {
+                //        result = await _roleManager.RemoveClaimAsync(role, roleClaims.Where(c => c.Value == claim).FirstOrDefault());
+                //        if (!result.Succeeded)
+                //            return Tuple.Create(false, result.Errors.Select(e => e.Description).ToArray());
+                //    }
+                //}
+
+                //if (claimsToAdd.Any())
+                //{
+                //    foreach (string claim in claimsToAdd)
+                //    {
+                //        result = await _roleManager.AddClaimAsync(role, new Claim(CustomClaimTypes.Permission, ApplicationPermissions.GetPermissionByValue(claim)));
+                //        if (!result.Succeeded)
+                //            return Tuple.Create(false, result.Errors.Select(e => e.Description).ToArray());
+                //    }
+                //}
+            }
+
+            return Tuple.Create(true, new string[] { });
+        }
+
+        public async Task<Tuple<bool, string[]>> UpdateUserAsync(UserDTO user)
+        {
+            return await UpdateUserAsync(user, null);
+        }
+
+        public async Task<Tuple<bool, string[]>> UpdateUserAsync(UserDTO user, IEnumerable<string> roles)
+        {
+            ObjectMapper mapper = new ObjectMapper();
+            ApplicationUser appUser = mapper.ConvertUserToIdentityUser(user);
+
+            var result = await _appUserManager.UpdateAsync(appUser);
+            if (!result.Succeeded)
+                return Tuple.Create(false, result.Errors.ToArray());
+
+
+            if (roles != null)
+            {
+                var userRoles = await _appUserManager.GetRolesAsync(user.Id);
+
+                var rolesToRemove = userRoles.Except(roles).ToArray();
+                var rolesToAdd = roles.Except(userRoles).Distinct().ToArray();
+
+                if (rolesToRemove.Any())
+                {
+                    result = await _appUserManager.RemoveFromRolesAsync(user.Id, rolesToRemove);
+                    if (!result.Succeeded)
+                        return Tuple.Create(false, result.Errors.ToArray());
+                }
+
+                if (rolesToAdd.Any())
+                {
+                    result = await _appUserManager.AddToRolesAsync(user.Id, rolesToAdd);
+                    if (!result.Succeeded)
+                        return Tuple.Create(false, result.Errors.ToArray());
+                }
+            }
+
+            return Tuple.Create(true, new string[] { });
         }
     }
 }
