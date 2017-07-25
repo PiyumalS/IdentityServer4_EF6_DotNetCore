8f90fb3d61a4cb3d81d8a846c1044f4791619ce0
nilendrat <nilendrat@gmail.com>
2017-07-25 13:47:45 +0530

    Added Missing Authorization handler

---
+++ SecureApiIdentityServer/AuthorizationHandler/HasPermissionRequirement.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureApiIdentityServer.AuthorizationHandler
+{
+    public class HasPermissionRequirement : IAuthorizationRequirement
+    {
+        public string Permission { get; private set; }
+
+        public HasPermissionRequirement(string permission)
+        {
+            this.Permission = permission;
+
+        }
+    }
+
+
+    public class HasPermissionHandler : AuthorizationHandler<HasPermissionRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasPermissionRequirement requirement)
+        {
+            // Find the required permissions claim
+            bool isOk = false;
+
+            var user = context.User;
+
+
+            var claim = context.User.FindFirst(
+                c => c.Type == "permission"
+                && c.Value == requirement.Permission);
+
+            // Succeed if the permissions array contains the required permission
+            if (claim != null)
+                context.Succeed(requirement);
+
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ SecureApiIdentityServer/AuthorizationHandler/RequiresPermissionAttribute.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureApiIdentityServer.AuthorizationHandler
+{
+    public class RequiresPermissionAttribute : TypeFilterAttribute
+    {
+        public RequiresPermissionAttribute(string permission) : base(typeof(RequiresPermissionAttributeImpl))
+        {
+            Arguments = new[] { new HasPermissionRequirement(permission) };
+        }
+
+        private class RequiresPermissionAttributeImpl : Attribute, IAsyncResourceFilter, IAuthorizationRequirement
+        {
+            private readonly IAuthorizationService _authService;
+            private readonly HasPermissionRequirement _requiredPermission;
+
+            public RequiresPermissionAttributeImpl(IAuthorizationService authService, HasPermissionRequirement requiredPermission)
+            {
+                _authService = authService;
+                _requiredPermission = requiredPermission;
+            }
+
+            public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+            {
+                if (!await _authService.AuthorizeAsync(context.HttpContext.User, context.ActionDescriptor.ToString(), _requiredPermission))
+                {
+                    context.Result = new ChallengeResult();
+                    context.HttpContext.Response.StatusCode = 401;
+
+                    return;
+                }
+
+                await next();
+            }
+        }
+    }
+}
