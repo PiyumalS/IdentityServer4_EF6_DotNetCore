7a290b01c2fa3abce88d385dfba6a8dd8a98840d
PiyumalS <piyumalrc@gmail.com>
2017-08-16 14:17:59 +0530

    client updated

--- Angular2Client/app/config.js
+++ Angular2Client/app/config.js
@@ -9,6 +9,6 @@ Config.REVOCATION_ENDPOINT = "http://localhost:59330/connect/revocation";
 Config.USERINFO_ENDPOINT = "http://localhost:59330/connect/userinfo";
 Config.CLIENT_ID = "resourceOwner";
 Config.GRANT_TYPE = "password";
-Config.SCOPE = "api1 offline_access openid profile roles";
+Config.SCOPE = "api1 offline_access openid";
 Config.CLIENT_SECRET = "secret";
 //# sourceMappingURL=config.js.map
